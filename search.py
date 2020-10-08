80f8c6c93b91d7bfa47fa52a57000eebd857ba33
amolinarossi <amolinarossi@gmail.com>
2020-10-08 14:50:19 -0400

    Finished astar search
    also deleted agent file

--- search.py
+++ search.py
@@ -21,30 +21,27 @@ files and classes when code is run, so be careful to not modify anything else.
 
 import sys
 import argparse
-import time
-#import numpy
 
-from agent import Agent
 from maze import Maze
 import math
-from queue import PriorityQueue, Queue,LifoQueue
+from queue import PriorityQueue, Queue, LifoQueue
 
 
-from typing import Dict, List, Iterator, Tuple, TypeVar, Optional
+from typing import Dict, TypeVar, Optional
 T = TypeVar('T')
 
 Location = TypeVar('Location')
 
 
-def search(maze, agent, searchMethod):    
+def search(maze, searchMethod):    
     return {
         "depth": dfs,
         "breadth": bfs,
         "astar": astar
-    }.get(searchMethod)(maze,agent)
+    }.get(searchMethod)(maze)
 
 
-def dfs(maze, agent):
+def dfs(maze):
     frontier = LifoQueue()
     came_from: Dict[Location, Location] = {}
     path = []
@@ -65,7 +62,7 @@ def dfs(maze, agent):
     return path, 0
 
 
-def bfs(maze, agent):
+def bfs(maze):
     frontier = Queue()
     came_from: Dict[Location, Location] = {}
     path = []
@@ -86,25 +83,43 @@ def bfs(maze, agent):
     return path, 0
 
 
-def astar(maze, agent):
-    # TODO: Write your code here
+def astar(maze):
     frontier = PriorityQueue()
-    frontier.put(maze.getStart(),0)
-   # functionH =  h(maze.maze)
-    #functionG 
+    frontier.put(maze.getStart(), 0)
+    came_from: Dict[Location, Optional[Location]] = {}
+    cost_so_far: Dict[Location, float] = {}
+    came_from[maze.getStart()] = None
+    cost_so_far[maze.getStart()] = 0
+    path = []
+    total_cost = 0
+    while not frontier.empty():
+        current: Location = frontier.get()
+        path.append(current)
+        if current == maze.getGoal():
+            print("Reached",current)
+            total_cost = cost_so_far[current]
+            break
+        print('Current: ', current)
+        for next in maze.getNeighbors(current[0], current[1]):
+            new_cost = cost_so_far[current] + getCost(current, next)
+            print('   ', next)
+            if next not in cost_so_far or new_cost < cost_so_far[next]:
+                cost_so_far[next] = new_cost
+                priority = new_cost + heuristic(maze, next)
+                frontier.put(next, priority)
+                came_from[next] = current
+        
+    return path, total_cost
 
-    # return path, num_states_explored
-    return [], 0
 
-
-def h(maze, agent):
-    (x1, y1) = agent.getPosition()
+def heuristic(maze, current):
+    (x1, y1) = current
     (x2, y2) = maze.getGoal()
     return math.sqrt((x2-x1)**2 + (y2-y1)**2)
 
-def getCost(maze, position):
-    (x1, y1) = position
-    (x2, y2) = maze.getGoal()
+def getCost(current, next):
+    (x1, y1) = current
+    (x2, y2) = next
     return math.sqrt((x2-x1)**2 + (y2-y1)**2)
 
 
@@ -123,8 +138,7 @@ if __name__ == "__main__":
     file_name = args.filename
     method = args.search
     maze = Maze(file_name)
-    agent = Agent(maze)
-    path, total_cost = search(maze, agent, method)
+    path, total_cost = search(maze, method)
 
     print(file_name)
     print(method)
